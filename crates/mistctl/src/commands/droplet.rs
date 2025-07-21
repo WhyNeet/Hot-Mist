936e70e485917efd5f4fa23244fe2b9253abf535
WhyNeet <arc.software.x@gmail.com>
2025-07-21 22:40:08 +0300

    feat(mistctl): implement droplet execution command

--- crates/mistctl/src/commands/droplet.rs
+++ crates/mistctl/src/commands/droplet.rs
@@ -27,7 +27,12 @@ pub async fn droplet_cmd(command: DropletCommand) -> anyhow::Result<()> {
                 other => eprintln!("Failed to create droplet ({other})."),
             }
         }
-        DropletCommand::Execute { name } => todo!("exec: {name}"),
+        DropletCommand::Execute { name } => {
+            println!("Executing Droplet: {name}");
+            let stdout = execute_droplet(&name).await?;
+            println!("=== stdout ===");
+            println!("{stdout}");
+        }
     }
 
     Ok(())
@@ -41,4 +46,19 @@ pub async fn create_droplet(config: &RootConfig) -> anyhow::Result<StatusCode> {
     let response = request.send().await?;
 
     Ok(response.status())
+}
+
+pub async fn execute_droplet(name: &str) -> anyhow::Result<String> {
+    let client = Client::new();
+    let request = client.get(format!("http://0.0.0.0:8080/ctr/droplet/{name}/execute"));
+    let response = request.send().await?;
+    let status = response.status();
+
+    let contents = response.text().await?;
+
+    if !status.is_success() {
+        anyhow::bail!("Failed to execute droplet ({}):\n{}", status, contents);
+    }
+
+    Ok(contents)
 }
