8ed2d34bf89ef40ec8b3dc386468a9e2532e99be
WhyNeet <arc.software.x@gmail.com>
2025-07-21 22:40:52 +0300

    refactor(config): derive clone

--- crates/config/src/lib.rs
+++ crates/config/src/lib.rs
@@ -1,7 +1,7 @@
 use serde::{Deserialize, Serialize};
 use std::path::PathBuf;
 
-#[derive(Debug, Serialize, Deserialize)]
+#[derive(Debug, Serialize, Deserialize, Clone)]
 pub struct RootConfig {
     pub api_version: String,
     pub metadata: Metadata,
@@ -9,7 +9,7 @@ pub struct RootConfig {
     pub spec: Spec,
 }
 
-#[derive(Debug, Serialize, Deserialize)]
+#[derive(Debug, Serialize, Deserialize, Clone)]
 #[serde(tag = "kind", content = "spec")]
 pub enum Spec {
     Droplet {
@@ -31,25 +31,25 @@ impl Spec {
     }
 }
 
-#[derive(Debug, Serialize, Deserialize)]
+#[derive(Debug, Serialize, Deserialize, Clone)]
 pub struct SpecSecret {
     pub name: String,
     pub mount_path: PathBuf,
 }
 
-#[derive(Debug, Serialize, Deserialize)]
+#[derive(Debug, Serialize, Deserialize, Clone)]
 #[serde(untagged)]
 pub enum SpecSource {
     #[serde(rename = "File")]
     File { path: PathBuf },
 }
 
-#[derive(Debug, Serialize, Deserialize)]
+#[derive(Debug, Serialize, Deserialize, Clone)]
 pub struct Metadata {
     pub name: String,
 }
 
-#[derive(Debug, Serialize, Deserialize)]
+#[derive(Debug, Serialize, Deserialize, Clone)]
 pub struct SpecRuntime {
     pub env: Option<Vec<RuntimeEnv>>,
     pub filesystem: Option<Vec<RuntimeFilesystemMount>>,
@@ -57,13 +57,13 @@ pub struct SpecRuntime {
     pub resources: RuntimeResources,
 }
 
-#[derive(Debug, Serialize, Deserialize)]
+#[derive(Debug, Serialize, Deserialize, Clone)]
 pub struct RuntimeEnv {
     pub name: String,
     pub value: String,
 }
 
-#[derive(Debug, Serialize, Deserialize)]
+#[derive(Debug, Serialize, Deserialize, Clone)]
 pub struct RuntimeFilesystemMount {
     pub name: String,
     pub guest_path: String,
@@ -71,12 +71,12 @@ pub struct RuntimeFilesystemMount {
     pub read_only: bool,
 }
 
-#[derive(Debug, Serialize, Deserialize)]
+#[derive(Debug, Serialize, Deserialize, Clone)]
 pub struct RuntimeNetwork {
     pub allowed_hosts: Vec<String>,
 }
 
-#[derive(Debug, Serialize, Deserialize)]
+#[derive(Debug, Serialize, Deserialize, Clone)]
 pub struct RuntimeResources {
     pub memory: String,
     pub cpu: String,
